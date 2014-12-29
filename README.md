a7f91af839d2e55c107b3c37fba75a0809bd3fa8
andreyalex <andreyalek@gmail.com>
2014-12-29 22:08:24 +0000

    Added menu

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 golden-lion-tea
 ===============
+#menu {
+    clear: both;
+    overflow: hidden;
+}
+
+#menu .item {
+    float: left;
+    margin:10px;
+}
+
+#menu .search {
+    float: left;
+    margin:10px;
+}
+
+#menu .item a {
+    text-decoration: none;
+    color: #55ff55;
+    text-shadow: 4px 4px 2px #ffffff;
+    font-size: 20px;
+}
+
+#menu .item.tea {
+    background: url("/assets/img/14-512.png") no-repeat;
+    width: 75px;
+    height: 95px;
+    line-height: 95px;
+    text-align: center;
+}
+
+#menu .nav-justified {
+    margin-top: 60px;
+    text-align: center;
+    font-size: 20px;
+}
--- magaz.php
+++ magaz.php
@@ -6,122 +6,184 @@
     <link rel="stylesheet" type="text/css" href="/assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="/assets/flaticon/flaticon.css">
     <link rel="stylesheet" type="text/css" href="/assets/theme.css">
+    <script src="/assets/jquery/jquery-2.1.3.min.js"></script>
+    <script src="/assets/bootstrap/js/bootstrap.min.js"></script>
 </head>
+
 <body>
-
-<div id="pane" class="container">
-    <div class="row">
-        <div  class="col-lg-4">
-            <div id="logo">
-                <span class="flaticon flaticon-lion5"></span>
-            </div>
-            <!--<a href="/"><img src="/assets/img/logo.jpg"></a>-->
-        </div>
-
-        <div class="col-lg-4">
-            <div id="reg">
-                <a class="btn btn-lg btn-default" href="/reg.php">Регистрация</a>
+    <div id="pane" class="container">
+        <div class="row">
+            <div  class="col-lg-4">
+                <div id="logo">
+                    <span class="flaticon flaticon-lion5"></span>
+                </div>
                 <!--<a href="/"><img src="/assets/img/logo.jpg"></a>-->
             </div>
-        </div>
 
-        <div class="col-lg-4" >
-            <div id="socials">
-                <a class="item fb" target="_blank" href="http://facebook.com"></a>
-                <a class="item gg" target="_blank" href="http://google.com"></a>
-                <a class="item tw" target="_blank" href="http://twitter.com"></a>
-            </div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div id="pane-left" class="col-sm-4 col-lg-3">
-            <div id="widget-news" class="widget">
-                <div class="title"><h3>Новости</h3></div>
-                <div class="content">
-                    <div class="item"><a href="/">Новость1</a></div>
-                    <div class="item"><a href="/">Новость2</a></div>
-                    <div class="item"><a href="/">Новость3</a></div>
-                </div>
-            </div>
-            <div id="widget-latest" class="widget">
-                <div class="title"><h3>Новинки</h3></div>
-                <div class="content">
-                    <div class="item">
-                        <img class="image-thumb" src="/assets/img/3-512.png"><a href="/">Чай 1</a>
-                    </div>
-                    <div class="item">
-                        <img class="image-thumb" src="/assets/img/14-512.png"><a href="/">Чай 2</a>
-                    </div>
-                    <div class="item">
-                        <img class="image-thumb" src="/assets/img/153.jpg"><a href="/">Чай 3</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div id="pane-main" class="col-sm-8 col-lg-9">
-            <div class="article">
-                <div class="image"><img class="image-thumb-large" src="/assets/img/logo.jpg" /></div>
-                <div class="title"><h3>Название</h3></div>
-                <div class="text">
-                    Разновидности чая исчисляются неимоверным числом,
-                    каждый обладает своими уникальными свойствами и вкусовыми качествами.
-                    Ни для кого не секрет, что настоящий китайский чай нормализирует давление,
-                    продлевает жизнь и дарит бодрый заряд энергии на целый день.
-                    Golden Lion поможет Вам подобрать чай, который Вы обязательно полюбите.
-                    Чтобы всегда быть в курсе наших новостей, акций и розыгрышей призов,
-                    подписывайтесь на нашу официальную публичную страницу VK.
-                    Она указана в разделе "Ссылки".
+            <div class="col-lg-4">
+                <div id="reg">
+                    <a class="btn btn-lg btn-default" href="/reg.php">Регистрация</a>
+                    <!--<a href="/"><img src="/assets/img/logo.jpg"></a>-->
                 </div>
             </div>
 
-            <div class="article odd">
-                <div class="image"><img class="image-thumb-large" src="/assets/img/logo.jpg" /></div>
-                <div class="title"><h3>Название</h3></div>
-                <div class="text">
-                    Разновидности чая исчисляются неимоверным числом,
-                    каждый обладает своими уникальными свойствами и вкусовыми качествами.
-                    Ни для кого не секрет, что настоящий китайский чай нормализирует давление,
-                    продлевает жизнь и дарит бодрый заряд энергии на целый день.
-                    Golden Lion поможет Вам подобрать чай, который Вы обязательно полюбите.
-                    Чтобы всегда быть в курсе наших новостей, акций и розыгрышей призов,
-                    подписывайтесь на нашу официальную публичную страницу VK.
-                    Она указана в разделе "Ссылки".
+            <div class="col-lg-4" >
+                <div id="socials">
+                    <a class="item fb" target="_blank" href="http://facebook.com"></a>
+                    <a class="item gg" target="_blank" href="http://google.com"></a>
+                    <a class="item tw" target="_blank" href="http://twitter.com"></a>
                 </div>
             </div>
 
-            <div class="article">
-                <div class="image"><img class="image-thumb-large" src="/assets/img/logo.jpg" /></div>
-                <div class="title"><h3>Название</h3></div>
-                <div class="text">
-                    Разновидности чая исчисляются неимоверным числом,
-                    каждый обладает своими уникальными свойствами и вкусовыми качествами.
-                    Ни для кого не секрет, что настоящий китайский чай нормализирует давление,
-                    продлевает жизнь и дарит бодрый заряд энергии на целый день.
-                    Golden Lion поможет Вам подобрать чай, который Вы обязательно полюбите.
-                    Чтобы всегда быть в курсе наших новостей, акций и розыгрышей призов,
-                    подписывайтесь на нашу официальную публичную страницу VK.
-                    Она указана в разделе "Ссылки".
-                </div>
-            </div>
+            <div class="col-lg-8" >
 
-            <div class="article odd">
-                <div class="image"><img class="image-thumb-large" src="/assets/img/logo.jpg" /></div>
-                <div class="title"><h3>Название</h3></div>
-                <div class="text">
-                    Разновидности чая исчисляются неимоверным числом,
-                    каждый обладает своими уникальными свойствами и вкусовыми качествами.
-                    Ни для кого не секрет, что настоящий китайский чай нормализирует давление,
-                    продлевает жизнь и дарит бодрый заряд энергии на целый день.
-                    Golden Lion поможет Вам подобрать чай, который Вы обязательно полюбите.
-                    Чтобы всегда быть в курсе наших новостей, акций и розыгрышей призов,
-                    подписывайтесь на нашу официальную публичную страницу VK.
-                    Она указана в разделе "Ссылки".
-                </div>
+                <nav id="menu" class="navbar navbar-default pull-right">
+                    <div class="container-fluid">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                        </div>
+
+                        <!-- Collect the nav links, forms, and other content for toggling -->
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul class="nav navbar-nav">
+                                <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+                                <li><a href="#">Link</a></li>
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                                    <ul class="dropdown-menu" role="menu">
+                                        <li><a href="#">Action</a></li>
+                                        <li><a href="#">Another action</a></li>
+                                        <li><a href="#">Something else here</a></li>
+                                        <li class="divider"></li>
+                                        <li><a href="#">Separated link</a></li>
+                                        <li class="divider"></li>
+                                        <li><a href="#">One more separated link</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                            <form class="navbar-form navbar-left" role="search">
+                                <div class="form-group input-group">
+                                    <input type="text" class="form-control" placeholder="Search">
+                                    <span class="input-group-btn">
+                                        <button type="submit" class="btn btn-default">
+                                            <span class="glyphicon glyphicon-search"></span>
+                                        </button>
+                                    </span>
+                                </div>
+                            </form>
+                        </div><!-- /.navbar-collapse -->
+                    </div><!-- /.container-fluid -->
+                </nav>
             </div>
         </div>
     </div>
 
+    <div id="pane" class="container">
+        <div class="row">
+            <div id="pane-left" class="col-sm-2 col-lg-2">
+                <div id="widget-news" class="widget">
+                    <div class="title"><h3>Новости</h3></div>
+                    <div class="content">
+                        <div class="item"><a href="/">Новость1</a></div>
+                        <div class="item"><a href="/">Новость2</a></div>
+                        <div class="item"><a href="/">Новость3</a></div>
+                    </div>
+                </div>
+                <div id="widget-latest" class="widget">
+                    <div class="title"><h3>Новинки</h3></div>
+                    <div class="content">
+                        <div class="item">
+                            <img class="image-thumb" src="/assets/img/3-512.png"><a href="/">Чай 1</a>
+                        </div>
+                        <div class="item">
+                            <img class="image-thumb" src="/assets/img/14-512.png"><a href="/">Чай 2</a>
+                        </div>
+                        <div class="item">
+                            <img class="image-thumb" src="/assets/img/153.jpg"><a href="/">Чай 3</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
-</div>
+            <div id="pane-main" class="col-sm-10 col-lg-10">
+                <div class="article">
+                    <div class="image"><img class="image-thumb-large" src="/assets/img/logo.jpg" /></div>
+                    <div class="title"><h3>ПУЭР</h3></div>
+                    <div class="text">
+                        Разновидности чая исчисляются неимоверным числом,
+                        каждый обладает своими уникальными свойствами и вкусовыми качествами.
+                        Ни для кого не секрет, что настоящий китайский чай нормализирует давление,
+                        продлевает жизнь и дарит бодрый заряд энергии на целый день.
+                        Golden Lion поможет Вам подобрать чай, который Вы обязательно полюбите.
+                        Чтобы всегда быть в курсе наших новостей, акций и розыгрышей призов,
+                        подписывайтесь на нашу официальную публичную страницу VK.
+                        Она указана в разделе "Ссылки".
+                    </div>
+                    <div class="baton">
+                        <a class="btn btn-lg btn-default" href="/index.html">Подробнее...</a>
+                    </div>
+                </div>
+
+                <div class="article odd">
+                    <div class="image"><img class="image-thumb-large" src="/assets/img/logo.jpg" /></div>
+                    <div class="title"><h3>ДА ХУН ПАО</h3></div>
+                    <div class="text">
+                        Разновидности чая исчисляются неимоверным числом,
+                        каждый обладает своими уникальными свойствами и вкусовыми качествами.
+                        Ни для кого не секрет, что настоящий китайский чай нормализирует давление,
+                        продлевает жизнь и дарит бодрый заряд энергии на целый день.
+                        Golden Lion поможет Вам подобрать чай, который Вы обязательно полюбите.
+                        Чтобы всегда быть в курсе наших новостей, акций и розыгрышей призов,
+                        подписывайтесь на нашу официальную публичную страницу VK.
+                        Она указана в разделе "Ссылки".
+                    </div>
+                    <div class="baton2">
+                        <a class="btn btn-lg btn-default" href="/index.html">Подробнее...</a>
+                    </div>
+                </div>
+
+                <div class="article">
+                    <div class="image"><img class="image-thumb-large" src="/assets/img/logo.jpg" /></div>
+                    <div class="title"><h3>ТЕ ГУАНЬ ИНЬ</h3></div>
+                    <div class="text">
+                        Разновидности чая исчисляются неимоверным числом,
+                        каждый обладает своими уникальными свойствами и вкусовыми качествами.
+                        Ни для кого не секрет, что настоящий китайский чай нормализирует давление,
+                        продлевает жизнь и дарит бодрый заряд энергии на целый день.
+                        Golden Lion поможет Вам подобрать чай, который Вы обязательно полюбите.
+                        Чтобы всегда быть в курсе наших новостей, акций и розыгрышей призов,
+                        подписывайтесь на нашу официальную публичную страницу VK.
+                        Она указана в разделе "Ссылки".
+                    </div>
+                    <div class="baton">
+                        <a class="btn btn-lg btn-default" href="/index.html">Подробнее...</a>
+                    </div>
+                </div>
+
+                <div class="article odd">
+                    <div class="image"><img class="image-thumb-large" src="/assets/img/logo.jpg" /></div>
+                    <div class="title"><h3>ПРОЧИЕ УЛУНЫ</h3></div>
+                    <div class="text">
+                        Разновидности чая исчисляются неимоверным числом,
+                        каждый обладает своими уникальными свойствами и вкусовыми качествами.
+                        Ни для кого не секрет, что настоящий китайский чай нормализирует давление,
+                        продлевает жизнь и дарит бодрый заряд энергии на целый день.
+                        Golden Lion поможет Вам подобрать чай, который Вы обязательно полюбите.
+                        Чтобы всегда быть в курсе наших новостей, акций и розыгрышей призов,
+                        подписывайтесь на нашу официальную публичную страницу VK.
+                        Она указана в разделе "Ссылки".
+                    </div>
+                    <div class="baton2">
+                        <a class="btn btn-lg btn-default" href="/index.html">Подробнее...</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
